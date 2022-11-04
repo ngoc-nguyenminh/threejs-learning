cef5b0d774caa07494b0e0d9314502104ebb6247
Nguyen Minh Ngoc <ngoc.nguyenminhhcmut@gmail.com>
2022-11-04 14:50:39 +0700

    Scroll-based animation

--- src/script.js
+++ src/script.js
@@ -1,14 +1,11 @@
 import './style.css'
 import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import { FontLoader } from 'three/examples/jsm/loaders/FontLoader'
-import {TextGeometry} from 'three/examples/jsm/geometries/TextGeometry'
 import * as dat from 'dat.gui'
+import gsap from 'gsap'
 
 /**
- * Base
+ * Debug
  */
-// Debug
 const gui = new dat.GUI()
 
 const parameters = {
@@ -17,8 +14,14 @@ const parameters = {
 
 gui
     .addColor(parameters, 'materialColor')
+    .onChange(() => {
+        material.color.set(parameters.materialColor)
+        particleMaterial.color.set(parameters.materialColor)
+    })
 
-
+/**
+ * Base
+ */
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 
@@ -29,82 +32,68 @@ const scene = new THREE.Scene()
  * Textures
  */
 const textureLoader = new THREE.TextureLoader()
+const gradientTexture = textureLoader.load('textures/gradients/3.jpg')
+gradientTexture.magFilter = THREE.NearestFilter
 
+/**
+ * Objects
+ */
+const material = new THREE.MeshToonMaterial({ 
+    color: parameters.materialColor,
+    gradientMap: gradientTexture
+})
+
+const objectDistance = 4
+const mesh1 = new THREE.Mesh(
+    new THREE.TorusGeometry(1, 0.4, 16, 60),
+    material
+)
+
+const mesh2 = new THREE.Mesh(
+    new THREE.ConeGeometry(1, 2, 32),
+    material
+)
+
+const mesh3 = new THREE.Mesh(
+    new THREE.TorusKnotGeometry(1, 0.4, 100, 60),
+    material
+)
+scene.add(mesh1, mesh2, mesh3)
+const meshes = [mesh1, mesh2, mesh3]
+
+mesh1.position.y = - objectDistance * 0
+mesh2.position.y = - objectDistance * 1
+mesh3.position.y = - objectDistance * 2
+
+mesh1.position.x = 2
+mesh2.position.x = -2
+mesh3.position.x = 2
+
+/**
+ * Particles
+ */
+const particleCount = 400
+const positions = new Float32Array(particleCount * 3)
+for (let i = 0; i < particleCount; i++) {
+    positions[i * 3] = (Math.random() - 0.5) * 10
+    positions[i * 3 +1] = objectDistance * 0.5 - Math.random() * objectDistance * 3
+    positions[i * 3 + 2] = (Math.random() - 0.5) * 10
+}
+const particlesGeometry = new THREE.BufferGeometry()
+particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
+const particleMaterial = new THREE.PointsMaterial({
+    color: parameters.materialColor,
+    sizeAttenuation: true,
+    size: 0.03
+})
+const particles = new THREE.Points(particlesGeometry, particleMaterial)
+scene.add(particles)
 /**
  * Lights
  */
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.2)
-// scene.add(ambientLight)
-
-const pointLight = new THREE.PointLight(0Xff9000, 1, 10)
-pointLight.position.x  = 100
-pointLight.position.y  = 2
-pointLight.position.z  = 3
-scene.add(pointLight)
-// const pointLight = new THREE.DirectionalLight('#ff9000', 0.5)
-// scene.add(pointLight)
-const helper = new THREE.PointLightHelper(pointLight)
-// scene.add(helper)
-/**
- * net
-const geometry = new THREE.BoxBufferGeometry(.5, .5, .5)
-
-const boxMaterial = new THREE.MeshStandardMaterial({
-    color: '#ffffff'
-})
-
-const positions = new Float32Array(21 * 21 * 3)
-for (let i = 0; i <= 20; i++) {
-    for (let j = 0; j <= 20; j++) {
-        const col = new THREE.Mesh(
-            geometry,
-            boxMaterial
-        )
-        col.position.x = i - 10
-        col.position.y = 0
-        col.position.z = j - 10
-
-        // positions[((i * 21) + j) * 3] = i - 10
-        // positions[((i * 21) + j) * 3 + 1] = 0
-        // positions[((i * 21) + j) * 3 + 2] = j - 10
-
-        col.geometry.setAttribute('uv2', new THREE.BufferAttribute(col.geometry.attributes.uv.array, 2))
-        col.receiveShadow = true
-        scene.add(col)
-    }
-}
-*/
-const fontLoader = new FontLoader()
-fontLoader.load(
-    'fonts/helvetiker_regular.typeface.json',
-    (font) => {
-        const textGeometry = new TextGeometry(
-            'You Sucks',
-            {
-                font: font,
-                size: 0.5,
-                height: 0.2,
-                curveSegments: 5,
-                bevelEnabled: true,
-                bevelThickness: 0.03,
-                bevelSize: 0.02,
-                bevelOffset: 0,
-                bevelSegments: 4
-            }
-        )
-        // textGeometry.computeBoundingBox() //Bounding box surround object to help with fustrum culling (render objects only when it needed to)
-        // textGeometry.translate( //center object with bounding box in mind
-        //     - (textGeometry.boundingBox.max.x - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.y - 0.02) * 0.5,
-        //     - (textGeometry.boundingBox.max.z - 0.03) * 0.5
-        // )
-        textGeometry.center()
-
-        const textMaterial = new THREE.MeshStandardMaterial()
-        const text = new THREE.Mesh(textGeometry, textMaterial)
-        scene.add(text)
-    }
-)
+const directionalLight = new THREE.DirectionalLight('#ffffff', 1)
+directionalLight.position.set(1, 1, 0)
+scene.add(directionalLight)
 
 /**
  * Sizes
@@ -114,7 +103,6 @@ const sizes = {
     height: window.innerHeight
 }
 
-document.addEventListener('mousemove', onMouseMove, false);
 window.addEventListener('resize', () =>
 {
     // Update sizes
@@ -131,64 +119,85 @@ window.addEventListener('resize', () =>
 })
 
 /**
- * Mouse
- */
-const mouse = new THREE.Vector2()
-// window.addEventListener('mousemove', (event) => {
-//     mouse.x = event.clientX / sizes.width * 2 - 1 //Cursor from -1 to 1
-//     mouse.y = - (event.clientY / sizes.height * 2 - 1)
-// })
-/**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.set(-2, 1, 3)
-camera.lookAt(new THREE.Vector3(0,0,0))
-scene.add(camera)
+//Camera Group: Create a camera group so that we can enable parallax(parallax for group) and scrolling(for camera) at the same time
+const cameraGroup = new THREE.Group()
+scene.add(cameraGroup)
 
-// Controls
-const controls = new OrbitControls(camera, canvas)
-controls.enableDamping = true
+// Base camera
+const camera = new THREE.PerspectiveCamera(35, sizes.width / sizes.height, 0.1, 100)
+camera.position.z = 6
+cameraGroup.add(camera)
 
 /**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+    alpha: true //Enable see through canvas
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.shadowMap.enabled = false //Enable renderer to cast shadows
-renderer.shadowMap.type = THREE.PCFSoftShadowMap
 
+/**
+ * Scroll
+ */
+let scrollY = window.scrollY
+let currentSection = 0
+
+window.addEventListener('scroll', () => {
+    scrollY = window.scrollY
+    const newSection = Math.round(scrollY / sizes.height)
+    if (currentSection != newSection) {
+        currentSection = newSection
+        gsap.to(
+            meshes[currentSection].rotation,
+            {
+                duration: 2,
+                ease: 'power2.inOut',
+                x: '+= 6',
+                y: '+= 3',
+                z: '+= 1.5'
+            }
+        )
+    }
+})
+
+/**
+ * Cursor
+ */
+const cursor = {
+    x: 0,
+    y: 0
+}
+window.addEventListener('mousemove', (event) => {
+    cursor.x = event.clientX / sizes.width - 0.5
+    cursor.y = event.clientY / sizes.height - 0.5
+})
 /**
  * Animate
  */
 const clock = new THREE.Clock()
-
-function onMouseMove(event) {
-	event.preventDefault();
-	mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
-	mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
-    pointLight.position.x = mouse.x * 15
-    pointLight.position.z = - mouse.y * 15
-
-
-	// let vector = new THREE.Vector3(mouse.x, mouse.y, 0);
-	// vector.unproject(camera);
-	// let dir = vector.sub(camera.position).normalize();
-	// let distance = -camera.position.z / dir.z;
-	// let pos = camera.position.clone().add(dir.multiplyScalar(distance));
-	// pointLight.position.copy(pos);
-};
+let previousTime = 0
 
 const tick = () =>
 {
     const elapsedTime = clock.getElapsedTime()
+    const deltaTime = elapsedTime - previousTime
+    previousTime = elapsedTime
 
-    // Update controls
-    // controls.update()
+    for (const mesh of meshes) {
+        mesh.rotation.y += deltaTime * .1
+        mesh.rotation.x += deltaTime * .12
+    }
+    
+    camera.position.y = - scrollY / sizes.height * objectDistance
+
+    const parallaxX = cursor.x
+    const parallaxY = - cursor.y
+    cameraGroup.position.x += (parallaxX - cameraGroup.position.x) * 5 * deltaTime//easing motions
+    cameraGroup.position.y += (parallaxY - cameraGroup.position.y) * 5 * deltaTime
 
     // Render
     renderer.render(scene, camera)
